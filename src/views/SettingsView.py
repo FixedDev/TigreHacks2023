1b214cd6686d3147b0c3dd5243c5721d8cdc7927
Wilovy09 <srwilovy09@gmail.com>
2023-05-27 08:24:03 -0600

    act navbar

--- src/views/SettingsView.py
+++ src/views/SettingsView.py
@@ -5,7 +5,7 @@ from flet import *
 navbar = AppBar(
         title=ft.Text("EcoBalance"),
         actions=[
-            ft.IconButton(ft.icons.HOME, on_click=lambda e: e.page.go('/index')),
+            ft.IconButton(ft.icons.ARROW_BACK_ROUNDED, on_click=lambda e: e.page.go('/index')),
             ft.IconButton(ft.icons.SETTINGS_ROUNDED, on_click=lambda e: e.page.go('/settings')),
         ]
     )
