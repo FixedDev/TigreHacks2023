57e6fef637f9a040052fa0d378df03bce1f0ce3c
Gilberto Garcia <gilbertodamian14@gmail.com>
2023-05-27 12:31:14 -0600

    fix: Actually load the existing coordinates.

--- algorithm.py
+++ algorithm.py
@@ -16,6 +16,22 @@ class Point:
     def __eq__(self, other):
         return other.longitude == self.longitude and other.longitude == self.longitude
 
+    def distance(self, other):
+        """
+          Calculate the distance between two coordinates using the Haversine formula.
+          """
+        radius = 6371  # Radius of the Earth in kilometers
+        dlat = math.radians(other.latitude - self.latitude)
+        dlon = math.radians(other.longitude - self.longitude)
+
+        a = math.sin(dlat / 2) ** 2 + math.cos(math.radians(self.latitude)) * math.cos(math.radians(other.latitude)) * math.sin(
+            dlon / 2) ** 2
+
+        c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+        distance = radius * c
+
+        return distance
+
 
 class Shape:
     def is_in(self, point: Point):
@@ -54,7 +70,7 @@ def read_existing_coordinates(file_path):
         next(csv_reader)  # Skip the header line
         for row in csv_reader:
             lat, lon = map(float, row)
-            existing_coordinates.append((lat, lon))
+            existing_coordinates.append(Point(lat, lon))
     return existing_coordinates
 
 
@@ -76,21 +92,21 @@ def generate_new_coordinates(existing_coordinates: list[Point], num_points, min_
     while len(new_coordinates) < num_points and grid_points:
         # Randomly select a point from the grid
         index = random.randint(0, len(grid_points) - 1)
-        lat, lon = grid_points[index]
+        point = grid_points[index]
         grid_points.pop(index)  # Remove the selected point from the grid
 
         is_valid = True
 
         # Check if the new coordinate satisfies the minimum distance constraint
         for coord in existing_coordinates:
-            distance = calculate_distance(lat, lon, coord.latitude, coord.longitude)
+            distance = point.distance(coord)
             if distance < min_distance:
                 is_valid = False
                 break
 
         if is_valid:
-            new_coordinates.append(Point(lat, lon))
-            existing_coordinates.append(Point(lat, lon))  # Update existing coordinates with new ones
+            new_coordinates.append(point)
+            existing_coordinates.append(point)  # Update existing coordinates with new ones
 
     return new_coordinates
 
@@ -99,14 +115,11 @@ def generate_new_coordinates(existing_coordinates: list[Point], num_points, min_
 csv_file_path = 'coordinates.csv'
 
 # Read existing coordinates from the CSV file
-existing_coordinates_tuple = read_existing_coordinates(csv_file_path)
-
-# convert existing coordinates to points
-existing_coordinates = [Point(lat, lon) for lat in existing_coordinates_tuple[0] for lon in existing_coordinates_tuple[1]]
+existing_coordinates = read_existing_coordinates(csv_file_path)
 
 # Generate new coordinates
-num_points = 10000
-min_distance = 0.5  # Minimum distance in kilometers
+num_points = 1000
+min_distance = 1  # Minimum distance in kilometers
 new_coordinates = generate_new_coordinates(existing_coordinates, num_points, min_distance)
 
 # Create a map centered on Monterrey
