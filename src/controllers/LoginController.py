0b56f5e463d82eb4c780f85ea19e4285bb9e1619
Gilberto Garcia <gilbertodamian14@gmail.com>
2023-05-26 22:20:02 -0600

    feature: Add basic functions for the login controller

--- src/controllers/LoginController.py
+++ src/controllers/LoginController.py
@@ -7,23 +7,21 @@ class LoginController:
 		self.password_hash_algor = password_hash_algor
 
 	def user_exists(self, user_name):
-
-	##	self.db_access_object.search({name: user_name}) busqueda
-	## return si existe o no
+		# self.db_access_object.search({name: user_name}) busqueda
+		# return si existe o no
 
 	def login(self, user_name, password):
+		# db_access_object.search({name: user_name}).then(__check_if_valid)
+		# si es valido, devolver un SUCCESS, si el usuario no se encuentra, INVALID_USER
+		# si la contraseña no coincide devolver WRONG_PASSWD
+		# cualquier otro caso devolver ERROR
 
-	## db_access_object.search({name: user_name}).then(__check_if_valid)
-	## si es valido, devolver un SUCCESS, si el usuario no se encuentra, INVALID_USER
-	## si la contraseña no coincide devolver WRONG_PASSWD
-	## cualquier otro caso devolver ERROR
 	def __checkIfValid(self, hashed, user):
-
-	## user.password_hash == hashed
-	## devolver si concuerda o no
+		# user.password_hash == hashed
+		# devolver si concuerda o no
 
 	def __hash_password(self, password):
-## hashed= self.password_hash_algor.hash(password)
+		# hashed= self.password_hash_algor.hash(password)
 
 
 class LoginResult(Enum):
