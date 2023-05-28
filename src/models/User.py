937e7a7e8b3fca3d49fa26fc0b11896b9f3e48f9
Gilberto Garcia <gilbertodamian14@gmail.com>
2023-05-27 21:58:08 -0600

    feat: Abstract away the user search

--- src/models/User.py
+++ src/models/User.py
@@ -1,11 +1,31 @@
+from bson import ObjectId
+
+
+def fromDocument(document):
+    user = User(document['mobile_number'], document['_id'])
+    user.credit = document['credit']
+    user.verified = document['verified']
+    user.passwd_hash = document['salt']
+    user.passwd_hash = document['passwd']
+
+    return user
+
 class User:
-    def __init__(self, mobile_number):
+    def __init__(self, mobile_number, id=ObjectId()):
+        self.__id = id
         self.__mobile_number = mobile_number
-        self.credit = None
+        self.credit = 0
         self.verified = False
         self.passwd_salt = None
         self.passwd_hash = None
 
     def forDb(self):
-        return {"mobile_number": self.__mobile_number, "salt": self.passwd_salt, "passwd": self.passwd_hash,
+        return {"_id": self.__id, "mobile_number": self.__mobile_number, "salt": self.passwd_salt,
+                "passwd": self.passwd_hash,
                 "verified": self.verified, "credit": self.credit}
+
+    def id(self):
+        return self.__id
+
+    def mobile_number(self):
+        return self.__mobile_number
