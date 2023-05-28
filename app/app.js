42d365e610cb408f780aeac9cdee353e2ee002d7
Wilovy09 <srwilovy09@gmail.com>
2023-05-28 04:26:47 -0600

    update

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-console.log("Ok!!!")
+console.log("Ok!!!");
 // post a /internal_login
 // LoginResult.SUCCESS
 // LoginResult.WRONG_PASSWORD
@@ -7,25 +7,12 @@ console.log("Ok!!!")
 function senddata() {
     let input_number = document.getElementById("input_number").value;
     let input_password = document.getElementById("input_password").value;
-
-    const url = "http://localhost:5500/internal_login";
-      fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-      })
-        .then(response => response.json())
-        .then(data => {
-          // Maneja la respuesta del servidor
-          console.log(data);
-        })
-        .catch(error => {
-          // Maneja cualquier error de la petición
-          console.error('Error:', error);
-        });
-
-    document.getElementById("login_button");
+    const path = "http://127.0.0.1:5500/internal_login";
+    
+    fetch(path)
+    .then(res => res.json())
+    .then(res => console.log(res));
+    
     console.log(input_number, input_password);
 }
 
@@ -40,17 +27,17 @@ function register() {
     console.log("register");
 }
 
-function authgoogle(){
+function authgoogle() {
     document.getElementById("authgoogle");
     console.log("authgoogle");
 }
 
-function authfacebook(){
+function authfacebook() {
     document.getElementById("authfacebook");
     console.log("authfacebook");
 }
 
-function authapple(){
+function authapple() {
     document.getElementById("authapple");
     console.log("authapple");
 }
