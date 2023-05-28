694612c1a254d871eb773b9434bebf1d4a77edae
Wilovy09 <srwilovy09@gmail.com>
2023-05-28 03:44:33 -0600

    post

--- app/app.js
+++ app/app.js
@@ -1,7 +1,30 @@
 console.log("Ok!!!")
+// post a /internal_login
+// LoginResult.SUCCESS
+// LoginResult.WRONG_PASSWORD
+// LoginResult.USER_NOT_EXIST
+// LoginResult.ERROR
 function senddata() {
     let input_number = document.getElementById("input_number").value;
     let input_password = document.getElementById("input_password").value;
+
+    const url = "http://localhost:5500/internal_login";
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+      })
+        .then(response => response.json())
+        .then(data => {
+          // Maneja la respuesta del servidor
+          console.log(data);
+        })
+        .catch(error => {
+          // Maneja cualquier error de la petición
+          console.error('Error:', error);
+        });
+
     document.getElementById("login_button");
     console.log(input_number, input_password);
 }
